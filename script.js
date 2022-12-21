987a6bde77c82a8844b0dd9f646b2cc9a8e38dd2
David Oliveira <daviddenisson2014@gmail.com>
2022-12-20 21:26:25 -0300

    form integration

--- script.js
+++ script.js
@@ -1,4 +1,45 @@
-window.addEventListener('scroll', function(){
+
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js";
+import { getFirestore, setDoc, addDoc, doc, collection } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-firestore.js";
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyB-ydXjwl-z25YZX7AkBnnVDbJSyytnPqc",
+    authDomain: "portfolio-contact-1ad87.firebaseapp.com",
+    projectId: "portfolio-contact-1ad87",
+    storageBucket: "portfolio-contact-1ad87.appspot.com",
+    messagingSenderId: "853953222478",
+    appId: "1:853953222478:web:dc425d6230369387fdb01f",
+    measurementId: "G-VCBCEZ25ZB"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+
+window.addEventListener('load', () => {
+
+    toastr.options = {
+        "closeButton": true,
+        "debug": false,
+        "newestOnTop": false,
+        "progressBar": true,
+        "positionClass": "toast-bottom-right",
+        "preventDuplicates": false,
+        "onclick": null,
+        "showDuration": "300",
+        "hideDuration": "1000",
+        "timeOut": "5000",
+        "extendedTimeOut": "1000",
+        "showEasing": "swing",
+        "hideEasing": "linear",
+        "showMethod": "fadeIn",
+        "hideMethod": "fadeOut"
+    }
+
+})
+
+window.addEventListener('scroll', function () {
     let navbar = document.querySelector('.navbar');
 
     let windowPosition = window.scrollY > 0;
@@ -6,156 +47,158 @@ window.addEventListener('scroll', function(){
     navbar.classList.toggle('scroll', windowPosition);
 });
 
+
+
 const navbarInteractive = {
-    
-   
-    closeNavbar(){
+
+
+    closeNavbar() {
         let navbar = document.querySelector('.navbar-items');
 
-        if(navbar.classList.contains('show')){
+        if (navbar.classList.contains('show')) {
             navbar.classList.remove('show');
         }
-        else{
-           
+        else {
+
         }
     },
-    openNavbar(){
+    openNavbar() {
         let navbar = document.querySelector('.navbar-items');
 
-        if(!navbar.classList.contains('show')){
+        if (!navbar.classList.contains('show')) {
             navbar.classList.add('show');
         }
-        else{
-           
+        else {
+
         }
     }
 
 }
 
 const switchScroll = {
-    getScroll(value){
+    getScroll(value) {
         let container = value;
-        this.switchScroll(container);      
+        this.switchScroll(container);
     },
-    switchScroll(value){
+    switchScroll(value) {
         let container = value;
 
-        if(container.classList.contains('intro')){
+        if (container.classList.contains('intro')) {
             this.deleteScroll("intro");
-           
+
         }
 
-        if(container.classList.contains('home')){
+        if (container.classList.contains('home')) {
             this.deleteScroll("home");
-           
+
         }
 
 
-        if(container.classList.contains('about')){
+        if (container.classList.contains('about')) {
             this.deleteScroll("about");
-           
+
         }
 
-        if(container.classList.contains('experienceaa')){
+        if (container.classList.contains('experienceaa')) {
             this.deleteScroll("experiences");
-           
+
         }
 
-        if(container.classList.contains('service')){
+        if (container.classList.contains('service')) {
             this.deleteScroll("services");
-           
+
         }
 
 
-        if(container.classList.contains('projects')){
+        if (container.classList.contains('projects')) {
             this.deleteScroll("projects");
-           
+
         }
 
-        if(container.classList.contains('contact')){
+        if (container.classList.contains('contact')) {
             this.deleteScroll("contact");
-           
-        }        
+
+        }
 
     },
-    deleteScroll(value){
+    deleteScroll(value) {
         let deleteActiveNav = document.querySelector('.navbar-texts-link.active');
 
-        if(value == "intro"){
-            if(deleteActiveNav != null){
+        if (value == "intro") {
+            if (deleteActiveNav != null) {
                 deleteActiveNav.classList.remove('active');
                 let navActive = document.querySelector('#navbar-home');
                 navActive.classList.add('active');
             }
-            else{
+            else {
                 return;
             }
         }
 
-        if(value == "home"){
-            if(deleteActiveNav != null){
+        if (value == "home") {
+            if (deleteActiveNav != null) {
                 deleteActiveNav.classList.remove('active');
                 let navActive = document.querySelector('#navbar-home');
                 navActive.classList.add('active');
             }
-            else{
+            else {
                 return;
             }
         }
 
-        if(value == "about"){
-            if(deleteActiveNav != null){
+        if (value == "about") {
+            if (deleteActiveNav != null) {
                 deleteActiveNav.classList.remove('active');
                 let navActive = document.querySelector('#navbar-about');
                 navActive.classList.add('active');
             }
-            else{
+            else {
                 return;
             }
         }
 
-        if(value == "services"){
-            if(deleteActiveNav != null){
+        if (value == "services") {
+            if (deleteActiveNav != null) {
                 deleteActiveNav.classList.remove('active');
                 let navActive = document.querySelector('#navbar-services');
                 navActive.classList.add('active');
             }
-            else{
+            else {
                 return;
             }
         }
 
-        if(value == "experiences"){
-            if(deleteActiveNav != null){
+        if (value == "experiences") {
+            if (deleteActiveNav != null) {
                 deleteActiveNav.classList.remove('active');
                 let navActive = document.querySelector('#navbar-experiences');
                 navActive.classList.add('active');
             }
-            else{
+            else {
                 return;
             }
         }
 
 
-        if(value == "projects"){
-            if(deleteActiveNav != null){
+        if (value == "projects") {
+            if (deleteActiveNav != null) {
                 deleteActiveNav.classList.remove('active');
                 let navActive = document.querySelector('#navbar-projects');
                 navActive.classList.add('active');
             }
-            else{
+            else {
                 return;
             }
         }
 
 
-        if(value == "contact"){
-            if(deleteActiveNav != null){
+        if (value == "contact") {
+            if (deleteActiveNav != null) {
                 deleteActiveNav.classList.remove('active');
                 let navActive = document.querySelector('#navbar-contact');
                 navActive.classList.add('active');
             }
-            else{
+            else {
                 return;
             }
         }
@@ -164,74 +207,159 @@ const switchScroll = {
 
 
 
-       
-      
+
+
     },
 }
 
 const switchButton = {
 
-    getButton(button){
+    getButton(button) {
         let buttonCalled = button;
 
-        if(buttonCalled.classList.contains('active')){
+        if (buttonCalled.classList.contains('active')) {
             return;
         }
 
-        else{
-           switchButton.removeActive();
-           switchButton.modifyButton(buttonCalled);
+        else {
+            switchButton.removeActive();
+            switchButton.modifyButton(buttonCalled);
         }
     },
-    
-    modifyButton(button){
+
+    modifyButton(button) {
         let modifyButton = button;
         modifyButton.classList.add('active');
     },
 
-    removeActive(){
+    removeActive() {
         let remove = document.querySelector('.navbar-texts-link.active');
         remove.classList.remove('active');
     }
-    
+
 }
 
 const valideForm = {
-    getFormValues(){
 
-        let name = document.getElementById('name');
-        let email = document.getElementById('Email');
-        let phone = document.getElementById('phone');
+    validateFormValues() {
 
-        
+        let nameInput = document.getElementById('name');
+        let emailInput = document.getElementById('Email');
+        let phoneInput = document.getElementById('phone');
+        let message = document.getElementById('message')
 
-        this.validateFormValues(name.value,email.value,phone.value);
+        if ((nameInput.value).length <= 2) {
+            toastr.error('Invalid Name')
+            this.errorForm(nameInput);
+
+        }
+
+        else {
+
+            this.correctForm(nameInput);
+
+        }
+
+        if ((emailInput.value).length <= 2) {
+            toastr.error('Invalid E-mail')
+            this.errorForm(emailInput);
+
+        }
+
+        else {
+
+
+
+            if (validateEmail(emailInput.value) == false) {
+
+                toastr.error('Invalid E-mail')
+                this.errorForm(emailInput);
+            }
+
+            else {
+                this.correctForm(emailInput);
+            }
+        }
+
+        if ((phoneInput.value).length <= 9) {
+            toastr.error('Invalid Phone')
+            this.errorForm(phoneInput);
+        }
+
+        else {
+            this.correctForm(phoneInput);
+        }
+
+        if (!(nameInput.value).length <= 2 && validateEmail(emailInput.value) == false && (phoneInput.value).length <= 9) {
+            return;
+        }
+
+        else {
+            this.sendForm(nameInput.value, emailInput.value, phoneInput.value, message.value);
+        }
+
+
 
     },
-    
-    validateFormValues(name,email,phone){
 
-       if(name.length > 2){
-            
-       }
+    errorForm(input) {
+
+        let inputError = input;
+        let fatherElement = inputError.parentNode;
+        fatherElement.classList.add('error');
 
     },
 
-    errorForm(input){
+    correctForm(input) {
+        let inputValide = input;
+        let fatherElement = inputValide.parentNode;
+
+        if (fatherElement.classList.contains('error')) {
+            fatherElement.classList.remove('error');
+        }
+
+        else {
+            return;
+        }
+    },
+
+    sendForm(name, email, phone, message) {
+
+        let validateName = name;
+        let validateEmail = email;
+        let validatePhone = phone;
+        let validateMessage = message;
+
+        let today = new Date();
+        let dd = String(today.getDate()).padStart(2, '0');
+        let mm = String(today.getMonth() + 1).padStart(2, '0');
+        let yyyy = today.getFullYear();
+
+        today = dd + '/' + mm + '/' + yyyy;
+
+        addDoc(collection(db, "contact"), {
+            name: validateName,
+            email: validateEmail,
+            phone: validatePhone,
+            message: validateMessage,
+            data: today,
+        })
+
+        toastr["success"]("Message sent")
 
     }
 }
 
-
 function switchTheme(button) {
+
     let buttonCall = button;
-   
+
     if (buttonCall.classList.contains("dark")) {
 
         let body = document.querySelector('body');
 
         if (body.classList.contains("dark-theme")) {
-           return;
+            return;
         }
 
         else if (body.classList.contains("light-theme")) {
@@ -263,81 +391,58 @@ function switchTheme(button) {
     }
 }
 
+function validateEmail(email) {
+    var re = /\S+@\S+\.\S+/;
+    return re.test(email);
+}
 
 const darkButton = document.querySelectorAll('i.fa-regular');
 darkButton.forEach(button => button.addEventListener('click', function () { switchTheme(button) }));
 
 const navbarLinkActive = document.querySelectorAll('.navbar-texts-link');
-navbarLinkActive.forEach(button => button.addEventListener('click', function(){ switchButton.getButton(button)}));
+navbarLinkActive.forEach(button => button.addEventListener('click', function () { switchButton.getButton(button) }));
 
 const scrollHoverContainer = document.querySelectorAll('.container');
-scrollHoverContainer.forEach(container => container.addEventListener('mouseover', function(){switchScroll.getScroll(container)}))
+scrollHoverContainer.forEach(container => container.addEventListener('mouseover', function () { switchScroll.getScroll(container) }))
 
 const buttonOpenNavbar = document.querySelector('.hamburguer-button');
-buttonOpenNavbar.addEventListener('click', function(){navbarInteractive.openNavbar()})
+buttonOpenNavbar.addEventListener('click', function () { navbarInteractive.openNavbar() })
 
 const buttonCloseNavbar = document.querySelector('.navbar-close');
-buttonCloseNavbar.addEventListener('click', function(){navbarInteractive.closeNavbar()})
+buttonCloseNavbar.addEventListener('click', function () { navbarInteractive.closeNavbar() })
 
 const formSend = document.querySelector('.button-form');
-formSend.addEventListener('click', function(){valideForm.getFormValues()})
+formSend.addEventListener('click', function () { valideForm.validateFormValues() })
 
 
 
- $(".owl-carousel").owlCarousel({
-                autoWidth:true,
-                margin:5,
-                loop: true,
-                responsive: {
-                    0: {
-                        items: 1
-                    },
-                    768: {
-                        items: 1
-                    },
-                    1000:{
-                        items:2
-                    },
-                    1280: {
-                        items: 3
-                    },
-                    1980: {
-                        items: 5
-                    }
 
-                },
-                autoplay:true,
-                autoplayTimeout: 1500,
-                
+$(document).ready(function () {
+    $(".owl-carousel").owlCarousel({
+        autoWidth: true,
+        margin: 5,
+        loop: true,
+        responsive: {
+            0: {
+                items: 1
+            },
+            768: {
+                items: 1
+            },
+            1000: {
+                items: 2
+            },
+            1280: {
+                items: 3
+            },
+            1980: {
+                items: 5
+            }
 
-            });
+        },
+        autoplay: true,
+        autoplayTimeout: 1500,
 
-            $(document).ready(function () {
-                $(".owl-carousel").owlCarousel({
-                    autoWidth:true,
-                    margin:5,
-                    loop: true,
-                    responsive: {
-                        0: {
-                            items: 1
-                        },
-                        768: {
-                            items: 1
-                        },
-                        1000:{
-                            items:2
-                        },
-                        1280: {
-                            items: 3
-                        },
-                        1980: {
-                            items: 5
-                        }
-    
-                    },
-                    autoplay:true,
-                    autoplayTimeout: 1500,
-                    
-    
-                });
-            })
+
+    });
+})
